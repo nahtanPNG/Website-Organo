d8e1882e0020a10c5f1a192328d349b62647db7a
nahtanpng <nathan.ferreiira13@gmail.com>
2024-06-27 14:22:05 -0300

    Banner Component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img
+        src="/images/banner.png"
+        alt="Um banner principal da página do Organo"
+      />
+    </header>
+  );
+}
+
+export default Banner;
